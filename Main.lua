b9c9b93cc1eae74de5a2c08f5059dfef8e801020
dvrk666 <lastnamenoname813@gmail.com>
2024-06-29 12:26:42 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/dvrk666/Private-menu/main/source.lua'))()
  
 local Window = Rayfield:CreateWindow({
     Name = "A|MZ PRIVATE CHEAT",
